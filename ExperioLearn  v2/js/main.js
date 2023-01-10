57c857105a466edbd4d642d451309ede45e085e3
Surfskills <ojjfred@gmail.com>
2023-01-10 16:47:04 +0300

    Front End

--- ExperioLearn  v2/js/main.js
+++ ExperioLearn  v2/js/main.js
@@ -58,3 +58,13 @@
     
 })(jQuery);
 
+    // Pills
+const triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
+triggerTabList.forEach((triggerEl) => {
+  const tabTrigger = new mdb.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', (e) => {
+    e.preventDefault()
+    tabTrigger.show()
+  })
+})(jQuery);
